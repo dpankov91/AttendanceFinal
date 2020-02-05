c2392fad8f2e7bb6879325eab12d36b46fe681fc
Domex99 <rave.monkey36@gmail.com>
2020-02-05 13:21:01 +0100

    ok

--- AttendanceProject/src/attendanceproject/AttendanceProject.java
+++ AttendanceProject/src/attendanceproject/AttendanceProject.java
@@ -19,7 +19,7 @@ public class AttendanceProject extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource(".fxml"));
         
         Scene scene = new Scene(root);
         
