d3ad0693e369c43718da4c4388f882e42ca5b791
dpankov91 <dpankov91@gmail.com>
2020-02-19 13:10:46 +0100

    Teacher Generate Key Window

---
+++ AttendanceProject/src/attendanceproject/gui/controllers/TeachersGenerateKeyWindowController.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package attendanceproject.gui.controllers;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author dpank
+ */
+public class TeachersGenerateKeyWindowController implements Initializable {
+
+    @FXML
+    private Label lblTodaysDate;
+    @FXML
+    private JFXTextField txtNewKey;
+    @FXML
+    private JFXButton btnSaveKey;
+    @FXML
+    private JFXButton btnBack;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void clickSaveNewKey(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickBack(ActionEvent event) {
+    }
+    
+}
