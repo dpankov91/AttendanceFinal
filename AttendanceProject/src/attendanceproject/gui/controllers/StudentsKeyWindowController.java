66f31e46ca93d3935272e7f999eeb454876afbf4
Tien0028 <tien0028@easv365.dk>
2020-03-24 14:21:02 +0100

    Update StudentsKeyWindowController.java

--- AttendanceProject/src/attendanceproject/gui/controllers/StudentsKeyWindowController.java
+++ AttendanceProject/src/attendanceproject/gui/controllers/StudentsKeyWindowController.java
@@ -74,7 +74,7 @@ public class StudentsKeyWindowController implements Initializable {
     private void clickEnterTodaysKey(ActionEvent event) {
       /* try{
            Connection con = Database.getConnection(url);
-           String query = "INSERT INTO"; //needs name of database here on both lines
+           String query = "INSERT txtTodaysKey.getText INTO"; //needs name of database here on both lines
            PreparedStatement pst = con.prepareStatement(query);
            pst.setString(1, txtTodaysKey.getText());
            
