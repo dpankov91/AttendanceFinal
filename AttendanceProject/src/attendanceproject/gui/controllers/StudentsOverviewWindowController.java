a5c40a805f7150c5cda70da40959d61f6af2a889
Domex99 <rave.monkey36@gmail.com>
2020-04-06 15:20:23 +0200

    some stuff in controller

--- AttendanceProject/src/attendanceproject/gui/controllers/StudentsOverviewWindowController.java
+++ AttendanceProject/src/attendanceproject/gui/controllers/StudentsOverviewWindowController.java
@@ -10,7 +10,10 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.PieChart;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * FXML Controller class
@@ -18,20 +21,35 @@ import javafx.scene.control.TableColumn;
  * @author hp
  */
 public class StudentsOverviewWindowController implements Initializable {
-
+    @FXML
+    private TableView<?> studentTableView;
     @FXML
     private TableColumn<?, ?> dateColumn;
     @FXML
     private TableColumn<?, ?> statusColumn;
     @FXML
     private PieChart statisticsPieChart;
+    @FXML
+    private Label lblStudentName;
+    
 
     /**
      * Initializes the controller class.
+     * @param url
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    
+    private void setUpTableView()
+    {
+        dateColumn.setCellValueFactory(new PropertyValueFactory<>("DATE"));
+        statusColumn.setCellValueFactory(new PropertyValueFactory<>("STATUS"));
+       
+        loadTableView();
+    }
+
+    private void loadTableView() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
